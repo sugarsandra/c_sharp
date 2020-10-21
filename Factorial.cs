6e72f7905f6dff330e1d1e0b34688238a3002cee
sugarsandra <45034972+sugarsandra@users.noreply.github.com>
2020-10-21 21:40:42 +0300

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+
+namespace LibraryParadigms
+{
+    class Program
+    {
+        public static int factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dose enan thetiko akeraio");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(factorial(x));
+        }
+
+    }
+}
+
+
